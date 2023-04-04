98cce2d05564a81a341abb258d4cd96c9200ac65
nandar-dev <mgnandar290@gmail.com>
2023-04-04 15:43:31 +0630

    update

---
+++ src/element/Drawer.vue
@@ -0,0 +1,84 @@
+<template>
+  <van-popup
+    @close="onClose"
+    v-model:show="show"
+    position="left"
+    :style="{ height: '100%', width: '60%' }">
+    <div class="content">
+      <div class="img-content">
+        <img src="./../assets/logo.png" alt="" />
+
+        <p>Version 1.0.1</p>
+      </div>
+
+      <div class="list">
+        <ul>
+          <li @click="goRoute('/')">Home</li>
+          <li @click="goRoute('/about')">About</li>
+        </ul>
+      </div>
+    </div>
+  </van-popup>
+</template>
+
+<script lang="ts">
+import router from '@/router';
+import { useRouter } from 'vue-router';
+export default {
+  props: {
+    show: {
+      type: Boolean,
+    },
+  },
+  emits: ["close"],
+
+  setup(context, emits) {
+    const onClose = () => {
+      emits.emit("close");
+    };
+
+    const goRoute = (name: any)=>{
+        router.push(name)
+    }
+
+    return {
+      onClose,
+      goRoute,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$imageWidth: 100px;
+.content {
+  padding: 10px;
+
+  .img-content {
+    width: $imageWidth;
+    margin: auto;
+
+    img {
+      width: $imageWidth;
+    }
+  }
+
+  .list {
+    ul {
+      li {
+        padding: 20px 0;
+        position: relative;
+
+        &::before {
+          content: "";
+          position: absolute;
+          bottom: 10px;
+          width: 100%;
+          height: 1px;
+          background: gainsboro;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/element/Menu.vue
+++ src/element/Menu.vue
@@ -1,42 +1,51 @@
 <template>
-    <van-popup v-model:show="showMenu" position="left" :style="{ height: '100%', width: '60%' }" />
-    <van-sticky>
-        <van-nav-bar :title="title">
-            <template #left>
-                <van-icon @click="menuHandler" name="wap-nav" />
-            </template>
-        </van-nav-bar>
-    </van-sticky>
+  <Drawer :show="showMenu" @close="onDrawerClose()" />
+  <van-sticky>
+    <van-nav-bar :title="title">
+      <template #left>
+        <van-icon @click="menuHandler" name="wap-nav" />
+      </template>
+    </van-nav-bar>
+  </van-sticky>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from 'vue'
+import { defineComponent, reactive, toRefs } from "vue";
+import Drawer from "./Drawer.vue";
 
 export default defineComponent({
-    props: {
-        title: {
-            type: String,
-        }
+  components: {
+    Drawer,
+  },
+  props: {
+    title: {
+      type: String,
     },
-    setup() {
-        const state = reactive({
-            showMenu: false
-        });
+  },
+  setup() {
+    const state = reactive({
+      showMenu: false,
+    });
 
-        const menuHandler = () => {
-            state.showMenu = true
-        }
+    const menuHandler = () => {
+      state.showMenu = true;
+    };
 
-        return {
-            ...toRefs(state),
-            menuHandler
-        }
-    },
-})
+    const onDrawerClose = () => {
+      state.showMenu = false;
+    };
+
+    return {
+      ...toRefs(state),
+      menuHandler,
+      onDrawerClose,
+    };
+  },
+});
 </script>
 
 <style scoped>
-.van-nav-bar{
-    box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+.van-nav-bar {
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
 }
 </style>
